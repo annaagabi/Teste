2718e377c1983097f9241dd23a7229cde6667d45
Anna Gabriela <124202500+annaagabi@users.noreply.github.com>
2023-10-10 18:59:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', (req,res) =>{
 })
 
 // entregar uma porta
-mongoose.connect('mongodb+srv://'+ process.env.DB_LOGIN + '' + ':' + process.env.DB_PASSWORD + '@blog.cadpuaq.mongodb.net/bancodedados?retryWrites=true&w=majority') // conectar ao banco de dados
+mongoose.connect('mongodb+srv://'+ process.env.DB_LOGIN + '' + ':' + process.env.DB_PASSWORD + '@blog.ipyjbzz.mongodb.net/?retryWrites=true&w=majority') // conectar ao banco de dados
 .then(() => {
     console.log("Conectamos ao mongo DB")
     app.listen(3000) // Vai ler a porta
